9d21c5ffb7d29f39af25332c500f93230684f9f1
Erick Floresca <160937182+erick-floresca-nttd@users.noreply.github.com>
2025-01-15 16:41:55 -0500

    Feature/init (#1)
    * clean copy
    
    * wip initial implementation
    
    * rename providers.tf so that it will be git ignored
    
    * fix naming
    
    * review changes
    
    * fix linting errors and add some validation
    
    * add provider version constraint
    
    * fix tflint warnings
    
    * tweak example
    
    * update after review
    
    * tweak tests
    
    * tweak tests
    
    * update test
    
    * upgrade golang and golanci-lint versions
    
    * cleanup code
    
    * tweak test
    
    * tweak example and revert post_deploy tests
    
    * update default for cors_allowed_origins
    
    * add locals.tf and update descriptions as per reviews
    
    * update tags

---
+++ main.tf
@@ -0,0 +1,76 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+resource "azurerm_signalr_service" "signalr" {
+  name                = var.signalr_name
+  location            = var.signalr_location
+  resource_group_name = var.resource_group_name
+
+  public_network_access_enabled = var.public_network_access_enabled
+  connectivity_logs_enabled     = var.connectivity_logs_enabled
+  http_request_logs_enabled     = var.http_request_logs_enabled
+  messaging_logs_enabled        = var.messaging_logs_enabled
+  service_mode                  = var.service_mode
+
+  sku {
+    name     = var.sku_name
+    capacity = var.sku_capacity
+  }
+
+  dynamic "cors" {
+    for_each = var.cors_allowed_origins != null ? ["cors"] : []
+    content {
+      allowed_origins = var.cors_allowed_origins
+    }
+  }
+
+  dynamic "live_trace" {
+    for_each = var.live_trace_enabled == true ? ["live_trace"] : []
+    content {
+      enabled                   = true
+      messaging_logs_enabled    = var.messaging_logs_enabled
+      connectivity_logs_enabled = var.connectivity_logs_enabled
+      http_request_logs_enabled = var.http_request_logs_enabled
+    }
+  }
+
+  dynamic "upstream_endpoint" {
+    for_each = var.upstream_endpoint != null ? ["upstream_endpoint"] : []
+    content {
+      category_pattern = upstream_endpoint.value["category_pattern"]
+      event_pattern    = upstream_endpoint.value["event_pattern"]
+      hub_pattern      = upstream_endpoint.value["hub_pattern"]
+      url_template     = upstream_endpoint.value["url_template"]
+    }
+  }
+
+  tags = merge(local.tags, { resource_name = var.signalr_name })
+}
+
+resource "azurerm_signalr_service_network_acl" "acl" {
+  for_each           = var.network_acl != null ? toset(["network_acl"]) : []
+  signalr_service_id = azurerm_signalr_service.signalr.id
+  default_action     = var.network_acl.default_action
+
+  public_network {
+    allowed_request_types = var.network_acl.allowed_request_types != null ? var.network_acl.allowed_request_types : null
+  }
+
+  dynamic "private_endpoint" {
+    for_each = var.private_endpoints != null ? var.private_endpoints : []
+    content {
+      id                    = private_endpoint.value.private_endpoint_id
+      allowed_request_types = private_endpoint.value.allowed_request_types
+    }
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,136 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+variable "resource_group_name" {
+  description = "Resource group name of the SignalR Service"
+  type        = string
+  nullable    = false
+}
+
+variable "signalr_location" {
+  description = "Location of the SignalR Service"
+  type        = string
+  nullable    = false
+}
+
+variable "signalr_name" {
+  description = "Name of the signalr to create"
+  type        = string
+  nullable    = false
+}
+
+variable "public_network_access_enabled" {
+  description = "Indicates whether public network access is allowed"
+  type        = bool
+  default     = true
+}
+
+variable "connectivity_logs_enabled" {
+  description = "Indicates whether to enable connectivity logs"
+  type        = bool
+  default     = false
+}
+
+variable "http_request_logs_enabled" {
+  description = "Indicates whether to enable http request logs"
+  type        = bool
+  default     = false
+}
+
+variable "live_trace_enabled" {
+  description = "Indicates whether to enable live traces"
+  type        = bool
+  default     = false
+}
+
+variable "messaging_logs_enabled" {
+  description = "Indicates whether to enable messaging logs"
+  type        = bool
+  default     = false
+}
+
+variable "service_mode" {
+  description = "The service mode of the SignalR Service. Possible values are Default, Classic, and Serverless"
+  type        = string
+  default     = "Default"
+
+  validation {
+    condition     = can(regex("^(Default|Classic|Serverless)$", var.service_mode))
+    error_message = "Invalid `service_mode` value"
+  }
+}
+
+variable "sku_name" {
+  description = "The SKU of the SignalR Service. Possible values are Free_F1, Standard_S1, Premium_P1, and Premium_P2"
+  type        = string
+  default     = "Free_F1"
+  validation {
+    condition     = can(regex("^(Free_F1|Standard_S1|Premium_P1|Premium_P2)$", var.sku_name))
+    error_message = "Invalid `sku_name` value"
+  }
+}
+
+variable "sku_capacity" {
+  description = "The capacity of the SKU.  See [the documentation](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/signalr_service#capacity-1) for possible values."
+  type        = number
+  default     = 1
+  validation {
+    condition = contains([
+      1, 2, 3, 4, 5, 6, 7, 8, 9,
+      10, 20, 30, 40, 50, 60, 70, 80, 90,
+    100, 200, 300, 400, 500, 600, 700, 800, 900, 1000], var.sku_capacity)
+    error_message = "Invalid `sku_capacity` value"
+  }
+}
+
+variable "cors_allowed_origins" {
+  description = <<EOF
+  The allowed origins for CORS, separated by comma. The default is set to ["*"] which will allow all origins
+  EOF
+  type        = list(string)
+  default     = ["*"]
+}
+
+variable "upstream_endpoint" {
+  description = "The upstream endpoint configuration"
+  type = object({
+    category_pattern = optional(list(string))
+    event_pattern    = optional(list(string))
+    hub_pattern      = optional(list(string))
+    url_template     = optional(string)
+  })
+  default = null
+}
+
+variable "network_acl" {
+  description = "The SignalR network ACL configuration"
+  type = object({
+    default_action        = string
+    allowed_request_types = list(string)
+  })
+  default = null
+}
+
+variable "private_endpoints" {
+  description = "The private endpoints for the SignalR network ACL"
+  type = list(object({
+    private_endpoint_id   = string
+    allowed_request_types = list(string)
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
